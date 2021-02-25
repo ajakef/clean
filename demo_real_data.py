66eb7241f9524f31f2d620ec306732afcf871431
Jake Anderson <ajakef@gmail.com>
2021-02-25 15:38:37 -0700

    added microbarom test

--- demo_real_data.py
+++ demo_real_data.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import obspy, scipy, importlib
+import obspy, scipy
 try:
+    import importlib
     importlib.reload(clean)
     print('reloaded')
 except:
@@ -90,3 +91,27 @@ clean.polar_freq_slow_spec(result, 'fh')
 plt.subplot(2,2,4)
 clean.polar_freq_slow_spec(result, 'fa')
 plt.tight_layout()
+
+#%% microbarom detection
+st = obspy.read('data/noise.mseed')
+st.filter('bandpass', freqmin=0.15, freqmax = 1)
+inv = obspy.read_inventory('data/XP_PARK_inventory.xml') # includes coordinates
+clean.add_inv_coords(st, inv) # store the coordinates in the stream
+
+## define slowness grid to search
+s_list = np.arange(-4, 4, 0.25)
+result = clean.clean(st, phi = 0.2, win_length_sec = 180, 
+                     freq_bin_width = 1, freq_min = 0, freq_max = 2, 
+                     sxList = s_list, syList = s_list, prewhiten = False)
+plt.figure(3)
+plt.subplot(2,2,1)
+clean.plot_freq_slow_spec(result, 'xy', 'original')
+plt.subplot(2,2,2)
+clean.plot_freq_slow_spec(result, 'xy')
+plt.subplot(2,2,3)
+clean.polar_freq_slow_spec(result, 'fh')
+plt.subplot(2,2,4)
+clean.polar_freq_slow_spec(result, 'fa')
+plt.tight_layout()
+
+
